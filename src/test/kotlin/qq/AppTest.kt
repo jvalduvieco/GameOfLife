29fb49eca8ce7f3477cb458035d6e0626d2da78f
Joan Valduvieco <jvalduvieco@gmail.com>
2019-06-07 18:37:10 +0100

    Revert "working"
    This reverts commit 53e6477b

--- src/test/kotlin/qq/AppTest.kt
+++ src/test/kotlin/qq/AppTest.kt
@@ -33,28 +33,30 @@ class AppTest {
     @Test
     fun `Can find neighbours of a central cell`() {
         assertEquals(listOf(
-                AbsoluteCoordinates(9, 9), AbsoluteCoordinates(10, 9), AbsoluteCoordinates(11, 9),
-                AbsoluteCoordinates(9, 10), AbsoluteCoordinates(11, 10),
-                AbsoluteCoordinates(9, 11), AbsoluteCoordinates(10, 11), AbsoluteCoordinates(11, 11)),
-                AbsoluteCoordinates(10, 10).neighbours())
+                Coordinates.AbsoluteCoordinates(9, 9), Coordinates.AbsoluteCoordinates(10, 9), Coordinates.AbsoluteCoordinates(11, 9),
+                Coordinates.AbsoluteCoordinates(9, 10), Coordinates.AbsoluteCoordinates(11, 10),
+                Coordinates.AbsoluteCoordinates(9, 11), Coordinates.AbsoluteCoordinates(10, 11), Coordinates.AbsoluteCoordinates(11, 11)),
+                Coordinates.AbsoluteCoordinates(10, 10).neighbours())
     }
 
-    data class AbsoluteCoordinates(val x: Int, val y: Int) {
+    class Coordinates {
+        data class AbsoluteCoordinates(val x: Int, val y: Int) {
 
-        private fun translate(other: RelativeCoordinates): AbsoluteCoordinates {
-            return AbsoluteCoordinates(x + other.x, y + other.y)
+            private fun translate(other: RelativeCoordinates): AbsoluteCoordinates {
+                return AbsoluteCoordinates(x + other.x, y + other.y)
+            }
+
+            fun neighbours(): List<AbsoluteCoordinates> {
+                return listOf(
+                        RelativeCoordinates(-1, -1), RelativeCoordinates(0, -1), RelativeCoordinates(1, -1),
+                        RelativeCoordinates(-1, 0), RelativeCoordinates(1, 0),
+                        RelativeCoordinates(-1, 1), RelativeCoordinates(0, 1), RelativeCoordinates(1, 1)
+                ).map { translate(it) }
+            }
         }
 
-        fun neighbours(): List<AbsoluteCoordinates> {
-            return listOf(
-                    RelativeCoordinates(-1, -1), RelativeCoordinates(0, -1), RelativeCoordinates(1, -1),
-                    RelativeCoordinates(-1, 0), RelativeCoordinates(1, 0),
-                    RelativeCoordinates(-1, 1), RelativeCoordinates(0, 1), RelativeCoordinates(1, 1)
-            ).map { translate(it) }
-        }
+        data class RelativeCoordinates(val x:Int, val y:Int)
     }
-
-    data class RelativeCoordinates(val x:Int, val y:Int)
 
     private fun survivesThisGeneration(isAlive: Boolean, aliveNeightbours: Int): Boolean {
         return when (isAlive) {
