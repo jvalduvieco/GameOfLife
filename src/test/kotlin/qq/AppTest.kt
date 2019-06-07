aa93c47f2d4c7b6e6809f679b7935c0daaa9453c
Joan Valduvieco <jvalduvieco@gmail.com>
2019-06-07 17:31:05 +0100

    Revert "working"
    This reverts commit 53e6477b

--- src/test/kotlin/qq/AppTest.kt
+++ src/test/kotlin/qq/AppTest.kt
@@ -8,23 +8,20 @@ import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class AppTest {
-    @Test fun `Any live cell with fewer than two live neighbours dies`() {
+    @Test fun `Any live cell with fewer than two live neighbours dies, as if caused by underpopulation`() {
         assertFalse(survivesThisGeneration(true, 1))
     }
 
-    @Test fun `Any live cell with more than one live neighbours lives`() {
-        assertTrue(survivesThisGeneration(true, 2))
-    }
-
-    @Test fun `Any live cell with three goes into next generation`() {
-        assertTrue(survivesThisGeneration(true, 3))
-    }
-
-    @Test fun `Any live cell with more than three alive neighbours dies`() {
+    @Test fun `Any live cell with more than three live neighbours dies, as if by overcrowding`() {
         assertFalse(survivesThisGeneration(true,4))
     }
 
-    @Test fun `Any dead cell with more than three a live neighbours becomes alive`() {
+    @Test fun `Any live cell with two or three live neighbours lives on to the next generation`() {
+        assertTrue(survivesThisGeneration(true, 2))
+        assertTrue(survivesThisGeneration(true, 3))
+    }
+
+    @Test fun `Any dead cell with exactly three live neighbours becomes a live cell`() {
         assertTrue(survivesThisGeneration(false,3))
     }
 
