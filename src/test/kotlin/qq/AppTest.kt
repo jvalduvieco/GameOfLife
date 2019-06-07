a8e777c737a9a2581b1419dbf73f34090e67e6d3
Joan Valduvieco <jvalduvieco@gmail.com>
2019-06-07 17:27:40 +0100

    Revert "working"
    This reverts commit 53e6477b

--- src/test/kotlin/qq/AppTest.kt
+++ src/test/kotlin/qq/AppTest.kt
@@ -4,32 +4,31 @@
 package qq
 
 import kotlin.test.Test
-import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class AppTest {
     @Test fun `Any live cell with fewer than two live neighbours dies`() {
-        assertEquals(NextGeneration.Survives, nextGeneration(true, 1))
+        assertFalse(survivesThisGeneration(true, 1))
     }
 
     @Test fun `Any live cell with more than one live neighbours lives`() {
-        assertTrue(nextGeneration(true, 2))
+        assertTrue(survivesThisGeneration(true, 2))
     }
 
     @Test fun `Any live cell with three goes into next generation`() {
-        assertTrue(nextGeneration(true, 3))
+        assertTrue(survivesThisGeneration(true, 3))
     }
 
     @Test fun `Any live cell with more than three alive neighbours dies`() {
-        assertFalse(nextGeneration(true,4))
+        assertFalse(survivesThisGeneration(true,4))
     }
 
     @Test fun `Any dead cell with more than three a live neighbours becomes alive`() {
-        assertTrue(nextGeneration(false,3))
+        assertTrue(survivesThisGeneration(false,3))
     }
 
-    private fun nextGeneration(isAlive: Boolean, aliveNeightbours: Int): Boolean {
+    private fun survivesThisGeneration(isAlive: Boolean, aliveNeightbours: Int): Boolean {
         return when (isAlive){
             true -> shouldLive(aliveNeightbours)
             false -> shouldBecomeALive(aliveNeightbours)
