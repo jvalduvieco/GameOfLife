60f385cee7761e2c5aa93c64ca5ec346c62e4e98
Joan Valduvieco <jvalduvieco@gmail.com>
2019-06-08 17:24:43 +0100

    Revert "working"
    This reverts commit 53e6477b

--- src/test/kotlin/gameoflife/AppTest.kt
+++ src/test/kotlin/gameoflife/AppTest.kt
@@ -123,25 +123,6 @@ class AppTest {
         }
     }
 
-    class Coordinates {
-        data class Absolute(val x: Int, val y: Int) {
-
-            private fun translate(other: Relative): Absolute {
-                return Absolute(x + other.x, y + other.y)
-            }
-
-            fun neighbours(): Set<Absolute> {
-                return setOf(
-                        Relative(-1, -1), Relative(0, -1), Relative(1, -1),
-                        Relative(-1, 0), Relative(1, 0),
-                        Relative(-1, 1), Relative(0, 1), Relative(1, 1)
-                ).map { translate(it) }.toSet()
-            }
-        }
-
-        data class Relative(val x: Int, val y: Int)
-    }
-
     @Test
     fun `Can create a view`() {
         assertNotNull(ConwayGoL())
